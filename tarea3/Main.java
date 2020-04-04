b30aeccc78b0b23f31f01a9ee327a616bce2bdb9
Jaime <56158062+jaimgarod10@users.noreply.github.com>
2020-04-04 12:26:03 +0200

    tarea3

---
+++ tarea3/Main.java
@@ -0,0 +1,20 @@
+package com.mycompany.tarea3;
+
+/**
+ *
+ * @author jaimg
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        tarea3 t = new tarea3();
+        t.setVisible(true);
+        
+    }
+    
+}
+
